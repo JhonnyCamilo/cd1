6a97ddf88ab7ab55b876de80825d3a63cac80674
JhonnyCamilo <jhonnybeltran57@gmail.com>
2021-10-23 05:12:36 -0500

    first commit

---
+++ src/main/java/tutoria/Interfaces/InterfaceFinca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tutoria.Interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+import tutoria.modelo.Finca;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceFinca extends CrudRepository<Finca,Integer>{
+    
+}
